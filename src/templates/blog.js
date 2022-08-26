cc38b869ac651b3575a293e395a6080e89d01c60
Julius Goddard <julius.goddard@webforprofessionals.com>
2022-08-26 15:39:32 +0100

    final commit

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import Layout from '../components/layout'
+import Layout from '../components/Layout'
 import { graphql } from 'gatsby'
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 import { renderRichText } from "gatsby-source-contentful/rich-text" 
