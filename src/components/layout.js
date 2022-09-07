e64fe2ce380e608eb45039cb8bc0d53198176c61
juliusgoddard <jkg@amalgatechnologies.com>
2022-09-07 10:29:55 +0100

    holiday work

--- src/components/layout.js
+++ src/components/layout.js
@@ -13,7 +13,6 @@ const Layout = (props) => {
         <div className={layoutStyles.container}>
             <div className={layoutStyles.content}>
             <Header />
-            <br />
             {props.children}
             </div>
             <Footer />
